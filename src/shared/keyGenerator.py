e5682c94f40c15d99c5ed177b8b89a81849d32bd
Camilo Pinilla <capinillab@unal.edu.co>
2018-09-28 01:38:09 -0500

    keyGenerator como modulo
    Se agrega un archivo para el algoritmo propio, el cual importa el 
    keyGenerator como un modulo, para mejorar la legibilidad del código.

--- src/shared/keyGenerator.py
+++ src/shared/keyGenerator.py
@@ -45,17 +45,17 @@ def keyGenerator(key, it):
 
 
 # MAIN
-msg = "camilo"
-key = "camilo"
-
-keyGen = keyGenerator(key, 4)
-print(keyGen)
-
-# Ejemplo de como evaluar cada caso
-for i in range(1):
-	for m, k in zip(msg, keyGen[i]):
-		vm = ord(m) # Valor ASCII de la letre en el mensaje
-		vk = ord(k) # Valor ASCII de la letre en la llave
-		nv = (ord(m)+ord(k))%256
-		nc = chr(nv) # Nuevo caracter ASCII, de reemplazo
-		print('{0:1} {1:1} {2:3d} {3:3d} {4:3d} {5:1}'.format(m, k, vm, vk, nv, nc))
+# msg = "camilo"
+# key = "camilo"
+#
+# keyGen = keyGenerator(key, 4)
+# print(keyGen)
+#
+# # Ejemplo de como evaluar cada caso
+# for i in range(1):
+# 	for m, k in zip(msg, keyGen[i]):
+# 		vm = ord(m) # Valor ASCII de la letre en el mensaje
+# 		vk = ord(k) # Valor ASCII de la letre en la llave
+# 		nv = (vm + vk) % 256
+# 		nc = chr(nv) # Nuevo caracter ASCII, de reemplazo
+# 		print('{0:1} {1:1} {2:3d} {3:3d} {4:3d} {5:1}'.format(m, k, vm, vk, nv, nc))
---
+++ src/shared/ownCipher.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Implementación de un algritmo de cifrado propio.
+
+import keyGenerator # Modulo encargado de generar las llaves, tomando comopartida la llave dada por el usuario.
+
+# MAIN
+msg = "camilo"
+key = "camilo"
+
+keyGen = keyGenerator.keyGenerator(key, 4)
+print(msg, key, keyGen)
+
+# Ejemplo de como evaluar cada caso
+for i in range(4):
+	nMsg = ""
+	for m, k in zip(msg, keyGen[i]):
+		vm = ord(m) # Valor ASCII de la letre en el mensaje
+		vk = ord(k) # Valor ASCII de la letre en la llave
+		nv = (vm + vk) % 256
+		nc = chr(nv) # Nuevo caracter ASCII, de reemplazo
+		print('{0:1} {1:1} {2:3d} {3:3d} {4:3d} {5:1}'.format(m, k, vm, vk, nv, nc))
+		nMsg += nc
+
+	msg = nMsg
+	print(nMsg)
