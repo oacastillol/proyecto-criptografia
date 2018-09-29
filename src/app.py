475394d9eeebfc60b157380817db43144b8140b5
alejandro castillo <oacastillol@gmail.com>
2018-09-29 03:51:16 -0500

    Prueba de front

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from .config import app_config
 from .models import db, bcrypt
 from .controllers.UserController import user_api as user_blueprint
@@ -12,20 +12,41 @@ def create_app(env_name):
   
   # app initiliazation
   app = Flask(__name__)
-
+  Bootstrap(app)
   app.config.from_object(app_config[env_name])
   bcrypt.init_app(app)
   db.init_app(app)
 
   app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
   app.register_blueprint(message_blueprint, url_prefix='/api/v1/messages')
-  app.register_blueprint(cipher_blueprint, url_prefix='/api/v1/cipher') 
+  app.register_blueprint(cipher_blueprint, url_prefix='/api/v1/cipher')
   
+  """
+PARTIAL FRONT
+  """
+  cache={'token':''}
   @app.route('/', methods=['GET'])
   def index():
     """
-    example endpoint
+    Load first page
     """
-    return 'Congratulations! Your first endpoint is workin'
-
+    return render_template("index.html",cache=cache)
+  @app.route('/login', methods=['GET'])
+  def login():
+    """
+    Load first page
+    """
+    return render_template("login.html",cache=cache)
+  @app.route('/logout', methods=['GET'])
+  def logout():
+    """
+    Load first page
+    """
+    return render_template("index.html",cache=cache)
+  @app.route('/messages', methods=['GET'])
+  def messages():
+    """
+    Load first page
+    """
+    return render_template("index.html",cache=cache)
   return app
