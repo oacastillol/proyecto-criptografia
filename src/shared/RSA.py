8a78bbc18894f482b4c8540101f73a816da0f61d
Camilo Pinilla <capinillab@unal.edu.co>
2018-12-02 09:26:33 -0500

    RSA implementación firma digital
    Se implementan los métodos, para la firma dígital, creación de la firma 
    usando valor del hash y llave privada
    validación de la firma comparando hash con valor de la firma y llave 
    publica

--- src/shared/RSA.py
+++ src/shared/RSA.py
@@ -48,7 +48,7 @@ def encrypt(m, en):
 
 # Descifrado de mensaje
 # IN -> c, mensaje a desifrar (conjunto valores enteros positivos)
-# IN -> dn ,llave privada de descifrado
+# IN -> dn , llave privada de descifrado
 # OUT -> m, mensaje descifrado
 def decrypt(c, dn):
 	m = []
@@ -59,6 +59,26 @@ def decrypt(c, dn):
 		m.append(z)
 
 	return m
+
+
+# Firma Digital
+# IN -> m, mensaje a cifrar (conjunto valores enteros positivos)
+# IN -> en, llave privada de cifrado
+# OUT -> y, mensaje cifrado
+def digitalSignature(hash, dn):
+	y = powerMod(hash, dn[0], dn[1])
+	# print ('hash:{0}, d:{1}, n:{2}, y:{3}'.format(hash, dn[0], dn[1], y))
+	return y
+
+
+# Validación de firma
+# IN -> x, y, valores para validación firma y firma cifrada
+# IN -> en, llave publica de descifrado
+# OUT -> True/False, resultado de la validación de la firma
+def signatureVerification(hash, y, en):
+	v = powerMod(y, en[0], en[1])
+	# print ('hash:{0}, y:{1}, e:{2}, n:{3}, v:{4}'.format(hash, y, en[0], en[1], v))
+	return True if v == hash else False
 
 
 # Algoritmo Euclidiano Extendido
@@ -106,21 +126,30 @@ def powerMod(a, b, n):
 
 # MAIN
 # Prueba del algoritmo
+# generación de llaves
 # Publish, Keep = MyOwnRSA()
-#
 # print ("Public Key:", Publish)
 # print ("Private Key: ", Keep)
 #
-# msg = "6882326879666683"
+# # Creación de mensaje de prueba en bloques tratables
+# msg = '6882326879666683'
 # m = []
-#
 # step = math.ceil(len(msg)/num_bloq)
 # for i in range(0, len(msg), step):
 # 	val = msg[i:i+step]
 # 	m.append(int(val))
-#
 # print ('msg', msg, m)
+#
+# # Pruebas de cifrado y descifrado se maneja una lista de valores para ello
 # c = encrypt(m, Publish)
-# print ('encriptado', c)
+# print ('cifrado', c)
 # m = decrypt(c, Keep)
-# print ('desencriptado', m)
+# print ('descifrado', m)
+#
+# # Pruebas de firma digital, un solo valor para firmar o validar
+# hash = 123
+# print ('hash: ', hash)
+# ds = digitalSignature(hash, Keep)
+# print ('firma digital: ', ds)
+# dv = signatureVerification(hash, ds, Publish)
+# print ('validación firma: ', dv)
